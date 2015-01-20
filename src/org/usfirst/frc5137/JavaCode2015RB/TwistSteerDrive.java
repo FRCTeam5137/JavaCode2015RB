3466d5307e483f8cd1482ef5d1d803a5aa47ba37
FRCTeam5137 <trevormcclellan@gmail.com>
2015-01-19 16:58:14 -0800

    Remapped X axis to Z axis

---
+++ src/org/usfirst/frc5137/JavaCode2015RB/TwistSteerDrive.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc5137.JavaCode2015RB;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.SpeedController;
+
+public class TwistSteerDrive extends RobotDrive {
+
+	public TwistSteerDrive(SpeedController leftMotor, SpeedController rightMotor) {
+		super(leftMotor, rightMotor);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+    /**
+     * Twist drive implements single stick driving.
+     * Given a single Joystick, the class assumes the Y axis for the move value and the Z axis
+     * for the rotate value.(Same as arcade drive, but uses Z axis/twist instead of x axis for rotation value.)
+     * @param stick The joystick to use for Arcade single-stick driving. The Y-axis will be selected
+     * for forwards/backwards and the Z-axis will be selected for rotation rate.
+     * @param squaredInputs If true, the sensitivity will be decreased for small values
+     */
+    public void twistDrive(GenericHID stick, boolean squaredInputs) {
+        // simply call the full-featured arcadeDrive with the appropriate values
+        arcadeDrive(stick.getY(), stick.getZ(), squaredInputs);
+    }
+	
+
+}
