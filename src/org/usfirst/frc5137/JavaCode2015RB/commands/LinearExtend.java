766b01293146230c71a585088812fbf36170df48
FRCTeam5137 <trevormcclellan@gmail.com>
2015-02-07 12:29:14 -0800

    Added support for gyro and grabber

--- src/org/usfirst/frc5137/JavaCode2015RB/commands/LinearExtend.java
+++ src/org/usfirst/frc5137/JavaCode2015RB/commands/LinearExtend.java
@@ -45,10 +45,12 @@ public class  LinearExtend extends Command {
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.cAHB20.stop();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
